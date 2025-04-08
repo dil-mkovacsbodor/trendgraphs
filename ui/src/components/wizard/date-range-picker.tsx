ac40df15b434348d7e623db0b0fde9ca4ea1c858
Attila Béli <abeli@diligent.com>
2025-04-08 22:44:13 +0200

    feat: added calendar and date range picker

---
+++ ui/src/components/wizard/date-range-picker.tsx
@@ -0,0 +1,66 @@
+import { Calendar } from '@/components/ui/calendar'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
+import { CalendarIcon } from 'lucide-react'
+import { format } from 'date-fns'
+import { cn } from '@/lib/utils'
+import type { UseFormReturn } from 'react-hook-form'
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import type { FormValues } from '@/lib/types/form'
+
+interface DateRangePickerProps {
+  form: UseFormReturn<FormValues>
+  name: 'timeframe'
+  label: string
+}
+
+export function DateRangePicker({ form, name, label }: DateRangePickerProps) {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <FormLabel className="text-sm font-medium text-gray-700">{label}</FormLabel>
+          <Popover>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant="outline"
+                  className={cn(
+                    'w-full mt-1 pl-3 text-left font-normal rounded-lg',
+                    !field.value && 'text-muted-foreground'
+                  )}
+                >
+                  {field.value?.from ? (
+                    field.value.to ? (
+                      <>
+                        {format(field.value.from, 'LLL dd, y')} -{' '}
+                        {format(field.value.to, 'LLL dd, y')}
+                      </>
+                    ) : (
+                      format(field.value.from, 'LLL dd, y')
+                    )
+                  ) : (
+                    <span>Pick a date range</span>
+                  )}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="range"
+                defaultMonth={field.value?.from}
+                selected={field.value}
+                onSelect={field.onChange}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
